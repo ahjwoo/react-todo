b2fd7c08b9366c93e4b65d534eb1c722a23f7fbb
Andrew Woo <ahjwoo@gmail.com>
2018-03-23 00:30:27 -0700

    Init Commit

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// load foundation
+$(document).foundation();
+// app css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <p>Boiler plate 3</p>,
+  document.getElementById('app')
+);
